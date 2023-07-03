f0916979f418843388b454f6235db129cd4df8a7
harkinder-dark <victorvaddely@gmail.com>
2023-07-03 14:47:07 +0000

    end public profil view

--- phi/models.py
+++ phi/models.py
@@ -34,8 +34,8 @@ class User(UserMixin, db.Model):
     twitter = db.Column(db.String, default='')
     website = db.Column(db.String, default='')
     password = db.Column(db.String, nullable=False)
-    posts = db.relationship('Post', backref='user')
-    comments = db.relationship('Comment', backref='user')
+    posts = db.relationship('Post', backref='user', cascade="all,delete")
+    comments = db.relationship('Comment', backref='user', cascade="all,delete")
 
     def __init__(self, **kwargs):
         """ initialisation """
@@ -71,7 +71,7 @@ class User(UserMixin, db.Model):
             if cmt.author == self.id:
                 all_cmts.append(cmt)
         return all_cmts
-        
+
 
 class Post(db.Model):
     """ post models """
@@ -79,7 +79,7 @@ class Post(db.Model):
     author = db.Column(db.String, db.ForeignKey(User.id), nullable=False)
     content = db.Column(db.Text, nullable=False)
     publish = db.Column(db.DATETIME, nullable=False, default=datetime.utcnow())
-    comments = db.relationship('Comment', backref='post')
+    comments = db.relationship('Comment', backref='post', cascade="all,delete")
 
     def __init__(self, **kwargs):
         """ initialisation """
@@ -126,3 +126,29 @@ class Comment(db.Model):
     def user_cmt(self):
         """ user comment """
         return User.query.get_or_404(self.author)
+
+
+class Settings(db.Model):
+    """ account settings """
+    id = db.Column(db.String, primary_key=True)
+    person = db.Column(db.String, db.ForeignKey(User.id), nullable=False)
+    hidden_fullname = db.Column(db.Boolean, default=False)
+    hidden_email = db.Column(db.Boolean, default=False)
+    hidden_country = db.Column(db.Boolean, default=False)
+    hidden_city = db.Column(db.Boolean, default=False)
+    hidden_job = db.Column(db.Boolean, default=False)
+    hidden_status = db.Column(db.Boolean, default=False)
+    hidden_society = db.Column(db.Boolean, default=False)
+    hidden_phone = db.Column(db.Boolean, default=True)
+    hidden_obbies = db.Column(db.Boolean, default=False)
+    hidden_cv = db.Column(db.Boolean, default=False)
+    hidden_social = db.Column(db.Boolean, default=False)
+    comment_disable = db.Column(db.Boolean, default=False)
+    
+    def __init__(self, **kwargs):
+        """ init """
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+        self.id = str(uuid4())
+        
+
--- phi/profil.py
+++ phi/profil.py
@@ -12,7 +12,7 @@ from werkzeug.security import (
     generate_password_hash,
     check_password_hash
 )
-from .models import User
+from .models import User, Post, Comment, Settings
 from . import app, db
 from .const import states
 
@@ -25,10 +25,12 @@ profile = Blueprint('profile', __name__)
 @login_required
 def overview():
     """ overviews """
-
+    posts = Post.query.filter_by(author=current_user.id).order_by(Post.publish)
     context = {
         'current_user': current_user,
-        'country': states
+        'country': states,
+        'posts': posts,
+        'settings':settings
     }
     return render_template('profil/update.html', **context)
 
@@ -75,7 +77,8 @@ def update():
         return redirect(url_for('profile.overview'))
     context = {
         'current_user': current_user,
-        'country': states
+        'country': states,
+        'settings':settings
     }
     return render_template('profil/update.html', **context)
 
@@ -86,14 +89,49 @@ def upload(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
     
 
-@profile.route('/<user_id>/settings', methods=['GET'], strict_slashes=False)
+@profile.route('/me/settings', methods=['GET', 'POST'], strict_slashes=False)
 @login_required
-def settings(user_id):
+def settings():
     """ settings """
+    settings = Settings.query.filter_by(person=current_user.id).first()
+    if request.method == 'POST':
+        person = current_user.id
+        hidden_fullname = True if request.form.get('hidden_fullname') == 'on'else False
+        hidden_email = True if request.form.get('hidden_email') == 'on'else False
+        hidden_country = True if request.form.get('hidden_country') == 'on'else False
+        hidden_city = True if request.form.get('hidden_city') == 'on'else False
+        hidden_job = True if request.form.get('hidden_job') == 'on'else False
+        hidden_status = True if request.form.get('hidden_status') == 'on'else False
+        hidden_society = True if request.form.get('hidden_society') == 'on'else False
+        hidden_phone = True if request.form.get('hidden_phone') == 'on'else False
+        hidden_obbies = True if request.form.get('hidden_obbies') == 'on'else False
+        hidden_cv = True if request.form.get('hidden_cv') == 'on'else False
+        hidden_social = True if request.form.get('hidden_social') == 'on' else False
+        comment_disable = True if request.form.get('comment_disable') == 'on' else False
+        settings = Settings(
+            person=person,
+            hidden_fullname=hidden_fullname,
+            hidden_email=hidden_email,
+            hidden_country=hidden_country,
+            hidden_city=hidden_city,
+            hidden_job=hidden_job,
+            hidden_status=hidden_status,
+            hidden_society=hidden_society,
+            hidden_phone=hidden_phone,
+            hidden_obbies=hidden_obbies,
+            hidden_cv=hidden_cv,
+            hidden_social=hidden_social,
+            comment_disable=comment_disable
+        )
+        
+        db.session.add(settings)
+        db.session.commit()
+        return redirect(url_for('profile.overview'))
     context = {
-        'current_user': current_user
+        'current_user': current_user,
+        'settings':settings
     }
-    return render_template('profil/settings.html', **context)
+    return render_template('profil/update.html', **context)
 
 
 @profile.route('/me/pwd', methods=['GET', 'POST'], strict_slashes=False)
@@ -114,23 +152,54 @@ def pwd():
         else:
             flash('password incorrect !!!')
     context = {
-        'current_user': current_user
+        'current_user': current_user,
+        'settings':settings
     }
     return render_template('profil/update.html', **context)
 
 
-@profile.route('/<user_id>/profil/rm', methods=['GET'], strict_slashes=False)
+@profile.route('/me/profil/rm', methods=['GET', 'POST'], strict_slashes=False)
 @login_required
-def rm(user_id):
+def rm():
     """ remove account """
+    if request.method == 'POST':
+        if request.form.get('yes'):
+            posts = Post.query.all()
+            cmts = Comment.query.all()
+            
+            # delete all post for current_user
+            for post in posts:
+                # delete all comments of post
+                for cmt in cmts:
+                    if cmt.postRef == post.id:
+                        db.session.delete(cmt)
+                # delete post
+                if post.author == current_user.id:
+                    db.session.delete(post)
+
+            #delete all comments for current_user
+            for cmt in cmts:
+                if cmt.author == current_user.id:
+                    db.session.delete(cmt)
+
+            db.session.delete(User.query.get_or_404(current_user.id))
+            db.session.commit()
+            return redirect(url_for('auth.sign'))
     context = {
-        'current_user': current_user
+        'current_user': current_user,
+        'settings':settings
     }
     return render_template('profil/update.html', **context)
 
 
-@profile.route('/<username>/profil', methods=['GET'], strict_slashes=False)
+@profile.route('/users/<username>', methods=['GET'], strict_slashes=False)
 @login_required
-def view(username):
-    """ profil view """
-    return render_template('profil/view.html')
+def public(username):
+    """ public profil """
+    person = User.query.filter_by(username=username).first()
+    setting = Settings.query.filter_by(person=person.id).first()
+    context = {
+        'person': person,
+        'setting': setting
+    }
+    return render_template('profil/view.html', **context)
